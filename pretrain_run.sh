6c0967a54f82ef101b73109b74664c2e78d38057
Norman Wang <ning.wang6@gmail.com>
2024-03-19 13:05:34 +0800

    init

--- pretrain_run.sh
+++ pretrain_run.sh
@@ -1,12 +1,12 @@
 # Pretrain模型训练
-deepspeed --num_nodes=1 --num_gpus=8 dxm_llm_main.py \
+deepspeed --num_nodes=1 --num_gpus=2 dxm_llm_main.py \
     --train_mode pretrain \
     --model_name_or_path TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T \
-    --save_name model/model-pretrained \
-    --data_path data/FinCorpus_tokenized \
+    --save_name out/ \
+    --data_path data-out \
     --epochs 1 \
     --per_device_train_batch_size 4 \
-    --max_length 4096 \
+    --max_length 2048 \
     --ds_zero_stage 2 \
     --log_steps 2 \
     --save_steps 40 \
